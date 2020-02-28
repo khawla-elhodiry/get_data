c69de982e81f3f4e0afb696bbf28e191c62e62aa
khawla-elhodiry <58861307+khawla-elhodiry@users.noreply.github.com>
2020-02-28 23:23:00 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+import 'dart:async';
+import 'dart:convert';
+import "package:http/http.dart" as http;
+import 'package:flutter/material.dart';
+void main() async{
+  List data = await getData();
+  runApp(new MaterialApp(
+      title: 'get group',
+      home:new Scaffold(
+          appBar: AppBar(
+              title: new Text ('goups')
+          )
+          ,
+          body: new Center(
+              child:new ListView.builder(itemCount: data.length,
+                  itemBuilder: (BuildContext context, int Position){
+                    return new ListTile(
+                      title:new Text("${data[Position]['name']}",style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 22,
+                      ),)
+                      ,
+                      leading: new Text("${data[Position]['id']}",style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 22,
+                      ),),
+                    );
+                  }
+              )
+          )
+      )
+  )
+  );
+}
+Future <List> getData() async{
+  String url = 'http://cet.ahmad.ly/api/groups';
+  http.Response r= await http.get(url);
+  return json.decode(r.body);
+}
